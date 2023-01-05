3197b5aab837465f569fa44987e3cc8fa52fcdc0
ThrivingDep <119737884+ThrivingDep@users.noreply.github.com>
2023-01-06 01:23:47 +0530

    Task-11 Save user Details on crud crud

--- project1.js
+++ project1.js
@@ -1,9 +1,5 @@
 
 
-
-
-
-
 function saveinLocalStorage(event){
     event.preventDefault();
     const expense = event.target.expense.value;
@@ -14,16 +10,30 @@ function saveinLocalStorage(event){
         discription,
         category
     }
-    localStorage.setItem( myObj.discription,JSON.stringify(myObj));
 
-    displayUser(myObj)
+
+
+    axios.post(" https://crudcrud.com/api/49efc82b0e5644908d91615fb6292b23/ExpenseData",myObj)
+     .then((response)=>{
+        displayUser(response.data)
+        console.log(response) 
+     })
+     .catch((err)=>{
+        document.body.innerHTML = document.body.innerHTML + "<h4> Something went wrong </h4>"
+        console.log(err)
+     })
+
+
+   // localStorage.setItem( myObj.discription, JSON.stringify(myObj));
+
+   // displayUser(myObj)
 
 
 }
 
 
-window.addEventListener('DOMContentLoaded', (event) => { 
-    Object.keys(localStorage).forEach(key => {
+ window.addEventListener('DOMContentLoaded', (event) => { 
+     Object.keys(localStorage).forEach(key => {
         const user = JSON.parse(localStorage.getItem(key))
        // addNewLineElement(user)
        displayUser(user);
@@ -32,38 +42,37 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
 
 function displayUser(user){
-const parentNode = document.getElementById('Users');
-const childHTML = `<li id = ${user.discription} >${user.expense} - ${user.discription} - ${user.category}
-<button onClick = deleteUser('${user.discription}')>Delete</button>
+ const parentNode = document.getElementById('Users');
+ const childHTML = `<li id = ${user.discription} >${user.expense} - ${user.discription} - ${user.category}
+ <button onClick = deleteUser('${user.discription}')>Delete</button>
 <button onclick = edituser('${user.discription}','${user.expense}')>Edit</button> </li>`
 
-parentNode.innerHTML = parentNode.innerHTML + childHTML;
+ parentNode.innerHTML = parentNode.innerHTML + childHTML;
 
 
 
-}
+ }
 
 
+ function deleteUser(discription){
+     localStorage.removeItem(discription);
+    removeItemfromScreen(discription);
+ }
 
-function deleteUser(discription){
-    localStorage.removeItem(discription);
-   removeItemfromScreen(discription);
-}
 
-
-function removeItemfromScreen(discription){
+ function removeItemfromScreen(discription){
 
     const parentNode = document.getElementById('Users');
     const elem = document.getElementById(discription);
      parentNode.removeChild(elem);
 
-}
+ }
 
 
 
-function edituser(discription , expense){
- document.getElementById('id1').value = discription;
- document.getElementById('id0').value = expense;
+ function edituser(discription , expense){
+  document.getElementById('id1').value = discription;
+  document.getElementById('id0').value = expense;
 
-  deleteUser(discription);
-}
+   deleteUser(discription);
+ }
